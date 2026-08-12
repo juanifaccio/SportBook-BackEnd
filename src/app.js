6aa52709703909a14c1add98a677bd282df9fe3d
Juani Faccio <juanignaciofaccio26@gmail.com>
2026-08-12 03:13:23 -0300

    actualiza backend

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
 const tipoCanchaRoutes = require('./routes/tipoCancha.routes');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/tipos-cancha', tipoCanchaRoutes);
 
 app.get('/', (req, res) => {
-    res.json({ mensaje: 'SportBook Backend funcionando' });
+  res.json({ mensaje: 'SportBook Backend funcionando' });
 });
 
 app.listen(3000, () => {
-    console.log('Servidor ejecutándose en http://localhost:3000');
+  console.log('Servidor ejecutándose en http://localhost:3000');
 });
